26f7219df3727b695ac75100adc87cb6a06935a4
Vadim <vadim_popov@epam.com>
2023-06-05 22:41:22 +0200

    Task 04.04 and 04.05

--- src/labs/lab4.ts
+++ src/labs/lab4.ts
@@ -11,6 +11,34 @@ export interface DamageLogger {
     (reason: string): void;
 }
 
+// T 04.03.1
+interface Person {
+    name: string;
+    email: string;
+}
+
+// T 04.03.2
+interface Author extends Person {
+    numBooksPublished: number;
+}
+
+// T 04.03.3
+interface Librarian extends Person {
+    department: string;
+    assistCustomer: (custName: string, bookTitle: string) => void;
+}
+
+// T 04.05.1
+type BookProperties = keyof Book;
+
+// Task 04.05.2
+function getProperty(book: Book, prop: BookProperties): any {
+    const value = book[prop];
+
+    return typeof value === 'function' ? value.name : value;
+}
+
+// =======================
 export function lab4 (): void {
     // T 04.01.5
     // T 04.01.8
@@ -39,4 +67,39 @@ export function lab4 (): void {
         console.log(`Damaged: ${reason}`);
     };
     logDamage('missing front cover');
+
+    // Task 04.03.4
+    const favoriteAuthor: Author = {
+        name: 'Anna',
+        email: 'anna@example.com',
+        numBooksPublished: 2
+    };
+
+    // Task 04.03.5
+    const favoriteLibrarian: Librarian = {
+        name: 'Boris',
+        email: 'boris@example.com',
+        department: 'Classical Literature',
+        assistCustomer(custName, bookTitle): void {
+            console.log(`${custName} ${bookTitle}`);
+        },
+    };
+
+    // Task 04.04.1
+    const offer: any = {
+        book: {
+            title: 'Essential TypeScript',
+        },
+    };
+    // Task 04.04.2
+    console.log(offer.magazine);
+    console.log(offer.magazine?.getTitle());
+    console.log(offer.book.getTitle?.());
+    console.log(offer.book.authors?.[0]);
+    console.log(offer.book.authors?.[10]?.name);
+
+    // Task 04.05.3
+    console.log(getProperty(myBook, 'title'));
+    console.log(getProperty(myBook, 'markDamaged'));
+    // console.log(getProperty(myBook, 'isbn'));
 }
